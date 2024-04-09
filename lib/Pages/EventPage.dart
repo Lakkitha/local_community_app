d9192a6b8f319745e666b394fe03518fbe1f01da
Killer3001 <chamathdewmina25@gmail.com>
2024-04-09 13:09:42 +0530

    Navigation Commit
    Initial Implementation of the navigation and the main pages.

---
+++ lib/Pages/EventPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventPage extends StatefulWidget {
+  const EventPage({super.key});
+
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text('Event Page'));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:local_community_app/themes/light_mode.dart';
 
+import 'Navigation/NavBar.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: NavBar(),
       debugShowCheckedModeBanner: false,
       title: 'Local Community',
       theme: LightMode,
