e45b34f16840a95050fb0bb3966ab0226c561efb
theRealUnd3rdog <kusalv123@gmail.com>
2024-04-10 18:05:27 +0530

    Gather user data on profile page and signout

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:local_community_app/Pages/UserEvents.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../util/styled_button.dart';
+import '../database/userdb.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -11,29 +15,69 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Profile Page'),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => UserEvent(
-                    eventName: "Something",
-                    eventStartDate: "01/01/2069",
-                    eventEndDate: "02/01/2049",
-                    eventOrganizer: "Something",
-                    eventImage: 'assets/images/img.jpg',
-                  ),
+    final String? uid = FirebaseAuth.instance.currentUser?.uid;
+
+    return Scaffold(
+      body: FutureBuilder<Map<String, dynamic>?>(
+        future: Database().getCurrentUserData(uid!),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center( // Wrap CircularProgressIndicator with Center widget
+              child: CircularProgressIndicator(), // Show a loading indicator while data is being fetched
+            );
+          }
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          if (snapshot.data == null) {
+            return Text('No user data found'); // Handle case where user data does not exist
+          }
+
+          final username = snapshot.data!['username'] as String;
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome $username buddy',
+                  style: TextStyle(fontSize: 22.0),
                 ),
-              );
-            },
-            child: const Text('Go to User Event'),
-          ),
-        ],
+                SizedBox(height: 15),
+                StyledButton(
+                  text: 'Sign Out',
+                  verticalPadding: 10,
+                  horizontalPadding: 20,
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.pushNamed(context, "/login");
+                  },
+                ),
+                SizedBox(height: 15),
+                StyledButton(
+                  text: 'My events',
+                  verticalPadding: 10,
+                  horizontalPadding: 20,
+                  onPressed: () 
+                  {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => UserEvent(
+                          eventName: "Something",
+                          eventStartDate: "01/01/2069",
+                          eventEndDate: "02/01/2049",
+                          eventOrganizer: "Something",
+                          eventImage: 'assets/images/img.jpg',
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
