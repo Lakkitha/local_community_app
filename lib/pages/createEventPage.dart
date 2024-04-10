bfb851f65b3f1d96b9b0461b7d090e313d58fe82
theRealUnd3rdog <kusalv123@gmail.com>
2024-04-10 16:13:54 +0530

    Merged files again

---
+++ lib/pages/createEventPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateEventPage extends StatefulWidget {
+  const CreateEventPage({super.key});
+
+  @override
+  State<CreateEventPage> createState() => _CreateEventPageState();
+}
+
+class _CreateEventPageState extends State<CreateEventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text('Create Event Page'));
+  }
+}
