857a20adf935021f04f0a862885ef8431fe2d858
Lakkitha <66767342+Lakkitha@users.noreply.github.com>
2024-04-11 23:16:22 +0530

    Pagination for the EventPage

--- lib/pages/eventPage.dart
+++ lib/pages/eventPage.dart
@@ -11,8 +11,10 @@ class EventPage extends StatefulWidget {
 
 class _EventPageState extends State<EventPage> {
   int filtervalue = 0;
+  int _currentPage = 0;
+  int _pageSize = 10;
+  bool _isLoading = false;
   List<Map<String, dynamic>> events = [
-    // Add your events here
     {
       'eventName': 'Brithday Party',
       'eventStartDate': '01/01/2023',
@@ -25,38 +27,79 @@ class _EventPageState extends State<EventPage> {
           'ut enim ad minim veniam quis nostrud exercitation ullamco laboris',
     },
     {
-      'eventName': 'Event 2',
+      'eventName': 'Brithday Party',
       'eventStartDate': '01/01/2023',
       'eventEndDate': '02/01/2023',
-      'eventOrganizer': 'Organizer 2',
-      'eventImage': 'assets/images/img.jpg',
-      'eventLocation': 'Location 2',
+      'eventOrganizer': 'Anya',
+      'eventImage': 'assets/images/pic5.jpg',
+      'eventLocation': 'Colombo, Sri Lanka',
       'eventDetails': 'lorem ipsum dolor sit amet consectetur adipiscing elit'
           'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
           'ut enim ad minim veniam quis nostrud exercitation ullamco laboris',
     },
-    {
-      'eventName': 'Event 3',
-      'eventStartDate': '01/01/2023',
-      'eventEndDate': '02/01/2023',
-      'eventOrganizer': 'Organizer 3',
-      'eventImage': 'assets/images/img.jpg',
-      'eventLocation': 'Location 3',
-      'eventDetails': 'lorem ipsum dolor sit amet consectetur adipiscing elit'
-          'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
-          'ut enim ad minim veniam quis nostrud exercitation ullamco laboris',
-    },
+    // Add your events here
+    // Assuming you have more events...
   ];
+
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      // reached the bottom
+      _loadMoreData();
+    }
+  }
+
+  void _loadMoreData() async {
+    if (!_isLoading) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      // Simulate loading delay
+      await Future.delayed(Duration(seconds: 2));
+
+      setState(() {
+        _currentPage++;
+        _isLoading = false;
+      });
+    }
+  }
+
+  List<Map<String, dynamic>> getPaginatedEvents() {
+    int startIndex = _currentPage * _pageSize;
+    int endIndex = startIndex + _pageSize;
+    if (startIndex >= events.length) {
+      return [];
+    }
+    return events.sublist(startIndex, endIndex.clamp(0, events.length));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Events",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16.0,
-            )),
+        title: Text(
+          "Events",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
         automaticallyImplyLeading: false,
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(50.0),
@@ -90,6 +133,28 @@ class _EventPageState extends State<EventPage> {
             ),
           ),
         ),
+      ),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: getPaginatedEvents().length + (_isLoading ? 1 : 0),
+        itemBuilder: (BuildContext context, int index) {
+          if (index == getPaginatedEvents().length && _isLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            final event = getPaginatedEvents()[index];
+            return EventCard(
+              eventName: event['eventName'],
+              eventStartDate: event['eventStartDate'],
+              eventEndDate: event['eventEndDate'],
+              eventOrganizer: event['eventOrganizer'],
+              eventImage: event['eventImage'],
+              eventLocation: event['eventLocation'],
+              eventDetails: event['eventDetails'],
+            );
+          }
+        },
       ),
     );
   }
